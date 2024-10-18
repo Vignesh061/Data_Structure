e63ba8bf81e24983471443f891ed32587f435b0e
Vignesh061 <vigneshvicky66005@gmail.com>
2024-10-18 21:20:33 +0530

    Created

---
+++ Depth_First_Search(DFS).c
@@ -0,0 +1,42 @@
+// Depth_First_Search(DFS)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int visited[MAX];
+int adj[MAX][MAX];
+int n;
+
+void DFS(int v) {
+    printf("%d ", v);
+    visited[v] = 1;
+
+    for(int i = 0; i < n; i++) {
+        if(adj[v][i] == 1 && !visited[i]) {
+            DFS(i);
+        }
+    }
+}
+
+int main() {
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter the adjacency matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    printf("DFS Traversal starting from vertex 0:\n");
+    DFS(0);
+
+    return 0;
+}
